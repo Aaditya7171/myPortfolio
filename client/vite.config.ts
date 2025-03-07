e20102ec5a92b562bfd3ec441b14a538ec5a718c
Aaditya7171 <risingaditya91@gmail.com>
2025-03-07 22:48:51 +0530

    Updated repo config for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/ProfessionalPortfolio/',
+    base: '/myPortfolio/',
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
